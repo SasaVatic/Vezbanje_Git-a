7f8c9b4f4320e2149e707c6b8de6396179cd6926
SasaV <zeklbike@gmail.com>
2021-03-05 20:45:41 +0100

    Napravljen Alat model

---
+++ Vezbanje_Git-a/Models/Alat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vezbanje_Git_a.Models
+{
+    public class Alat
+    {
+        public string Naziv { get; set; }
+        public string Tip { get; set; }
+        public int Kolicina { get; set; }
+    }
+}
